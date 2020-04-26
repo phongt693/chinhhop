b76984fe07bc43dba3835188139d47dbf69ee47c
phongt693 <42069420+phongt693@users.noreply.github.com>
2020-04-26 09:35:59 +0700

    Add files via upload
    liet ke  chinh hop

---
+++ chinhhop.js
@@ -0,0 +1,63 @@
+//V8 3.31.1
+
+function printCombinations(array, k){
+    var combinations = [];
+    
+    
+    function run(level, start){
+    
+        for(var i=start; i < array.length - k + level + 1; i++){
+            combinations[level] = array[i];
+            
+            if(level < k - 1){
+                run(level + 1, i + 1);
+            } else {
+                document.write(combinations.join(" "));
+            }
+        }
+        
+    }
+
+    
+    run(0, 0);
+}
+
+
+
+printCombinations([1, 2, 3, 4], 3);
+
+document.write("------------");
+
+function printPermutations(array, k){
+    var combinations = [];
+    var indices = [];
+    
+    
+    function run(level, start){
+    
+        for(var i=0; i < array.length; i++){
+            
+            if(!indices[i]){
+            
+                indices[i] = true;
+                
+                combinations[level] = array[i];
+                
+                if(level < k - 1){
+                    run(level + 1, i + 1);
+                } else {
+                    document.write(combinations.join(" "));
+                }
+                
+                indices[i] = false;
+            }
+        }
+        
+    }
+
+    
+    run(0, 0);
+}
+
+
+printPermutations([1, 2, 3, 4], 3);
